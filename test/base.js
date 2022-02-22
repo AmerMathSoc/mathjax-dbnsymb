507114deb9f6baa74be3854313e305010817a27a
Peter Krautzberger <p.krautzberger@gmail.com>
2022-02-22 10:30:10 +0100

    feat: BigCirc, botright, cappededge, crossing, openHopfUp

--- test/base.js
+++ test/base.js
@@ -10,6 +10,10 @@ const theBigString =
   $$G \\actsonleft X$$
   $$X \\actsonright G$$
   <p>\\( \\Associator \\) and \\( \\backoverslash \\) generate parenthesized tangles.</p>
+  $$x \\BigCirc y$$
+  $$\\sigma_yZ(\\OpenHopfUp_{\\!x}^y)  = \\Omega_y\\exp_\\#(\\botright_x^{\\!\\!y})$$
+  $$ {\\mathcal A}^w(\\cappededge)$$
+  $$(\\crossing)$$
   `
 
 fs.writeFileSync(__dirname+'/test.html' ,mj(theBigString, true));
